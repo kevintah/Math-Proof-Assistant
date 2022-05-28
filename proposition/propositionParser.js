ccf3614c1527ed7825a8d45e834e40e91a6b088b
kevintah <kevintah@yahoo.com>
2022-05-28 02:56:59 -0700

    It can do a basic integer proof

---
+++ proposition/propositionParser.js
@@ -0,0 +1,24 @@
+import{isInteger} from '../definitions/integers/integers.js';
+
+// Parse proposition
+
+
+function integerSubproof(proposition){
+var toParse = proposition;
+var parsed = toParse.split(" ");
+
+//sanity checks
+//console.log(parsed);
+//console.log(typeof(parsed[0]));
+
+if(typeof(parsed[0]) =='string' && isNaN(Number(parsed[0])) == false){
+  n = Number(parsed[0] && parsed[3]=='integer');
+}
+
+//write Proof
+var n = Number(parsed[0]);
+console.log(' ' + isInteger(n)) ;
+
+}
+
+export{integerSubproof};
--- whiteBoard/whiteBoard.js
+++ whiteBoard/whiteBoard.js
@@ -1,1 +1,29 @@
-console.log("Test");
+
+import{integerSubproof} from '../proposition/propositionParser.js';
+
+
+
+import rl from "readline-promise";
+const readline = rl.default;
+
+const rlp = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: true,
+});
+
+rlp.question(`Enter proposition   `, (proposition) => {
+  //proposition is shown
+
+      console.log('-----Worksheet-----')
+
+      // log user details
+      console.log(`Claim: \n ${proposition}`);
+      console.log("Proof:");
+      
+      integerSubproof(proposition);      
+      // close the stream
+      rlp.close();
+ 
+
+});
