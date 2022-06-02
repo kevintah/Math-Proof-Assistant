03aa2e3d65fdb3df04122cf892c93efc5565f885
kevintah <kevintah@yahoo.com>
2022-06-02 00:03:59 -0700

    2 files modified, 1 added, 1 deleted --working on parser and demorgan rules

---
+++ proposition/propositionParser.js
@@ -0,0 +1,6 @@
+import{proposition} from '../whiteBoard/whiteBoard.js';
+
+var split = proposition.split(" ");
+
+console.log(split);
+
--- whiteBoard/whiteBoard.js
+++ whiteBoard/whiteBoard.js
@@ -38,3 +38,5 @@ rlp.question(`Enter proposition   `, (proposition) => {
 
 
 });
+
+export{proposition};
