db7457b234adef71e15cfec97cccfce0e74b16bc
kevintah <kevintah@yahoo.com>
2022-06-02 14:13:01 -0700

    1 file modified -syntax --grammer --verification --restriction

--- proposition/propositionParser.js
+++ proposition/propositionParser.js
@@ -1,20 +1,36 @@
 import{proposition,statement,consequence} from '../whiteBoard/whiteBoard.js';
 
+
+
 var splitStatement;
 var splitConsequence;
-
+var regEx = /([^pq!=><)('=>'])/g;
 function statementSplitter(statement){
-
+  //verify syntax
+  while (statement !== null) {
+  if(regEx.test(statement) == true){
+    console.log('bad syntax');
+  }
+}
 splitStatement = statement.split(" ");
 splitStatement.unshift('if');
 console.log(splitStatement);
 }
 
 function consequenceSplitter(consequence){
+  //verify syntax
+  if (consequence !== null){
 
+  if(regEx.test(consequence) == true){
+    console.log('bad syntax');
+  }
+ }
   splitConsequence = consequence.split(" ");
   console.log(splitConsequence);
   }
+
+
+
 
 export{statementSplitter,consequenceSplitter,splitStatement,splitConsequence};
 
