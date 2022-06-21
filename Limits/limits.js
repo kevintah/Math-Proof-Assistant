45469286ac591bbe9cc72bdaff168970c3eda1eb
kevintah <kevintah@yahoo.com>
2022-06-21 16:01:27 -0700

    1 file added -- sketching limits.js

---
+++ Limits/limits.js
@@ -0,0 +1,14 @@
+//References
+//https://math.libretexts.org/Bookshelves/Calculus/Book%3A_Calculus_(OpenStax)/02%3A_Limits/2.2%3A_The_Limit_of_a_Function
+
+
+//strategy
+//check limit from the left
+//check limit from the right
+//check that limit from the right is the same as limit from the left 
+//if not then limit does not exist
+//Limit can be a number
+//limit can be infinity
+//limit can be zero
+
+
