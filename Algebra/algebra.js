1651f23989408e39c9b9728f1318c8c33f9315f6
kevintah <kevintah@yahoo.com>
2022-06-20 19:38:15 -0700

    2 files added -- creating simplify and expand functions

---
+++ Algebra/algebra.js
@@ -0,0 +1,11 @@
+//References
+//https://towardsdatascience.com/proving-inequalities-aab03b0588d5
+
+function simplify(){
+
+}
+
+function expand(){
+
+  
+}
---
+++ Bounds/Inequalities/inequalities.js
@@ -0,0 +1,2 @@
+//References
+//https://towardsdatascience.com/proving-inequalities-aab03b0588d5
